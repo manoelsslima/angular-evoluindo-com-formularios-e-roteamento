44b2461f89737ca27c2f7e943fccca2699da5346
Manoel Lima <manoel.lima@manaus.am.gov.br>
2025-12-18 13:15:25 -0400

    feat: enhance contact form with validation and error messages

--- indexa/src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ indexa/src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -20,9 +20,9 @@ export class FormularioContatoComponent {
 
   constructor() {
     this.contatoForm = new FormGroup({
-      nome: new FormControl(''),
-      telefone: new FormControl(''),
-      email: new FormControl(''),
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
       aniversario: new FormControl(''),
       redes: new FormControl(''),
       observacoes: new FormControl('')
@@ -30,7 +30,9 @@ export class FormularioContatoComponent {
   }
 
   salvarContato() {
-    console.log(this.contatoForm.value);
+    if (this.contatoForm.valid) {
+      console.log(this.contatoForm.value);
+    }
   }
 
   cancelar() {
